45797f898363ef9e19e1d02becd996ff00d1d995
brianxautumn <shred@brianautumn.com>
2020-12-21 10:38:34 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Vaporwavez
+        </h1>
+        <div>
+          <a href="https://www.instagram.com/vaporwavez_music/">Instagram</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
